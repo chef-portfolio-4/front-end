42a79cced5a51487cdb3edfb8cbf0ae9f533c363
nicanor-alicaya <donalicaya@gmail.com>
2020-01-07 12:18:59 -0500

    skeletal structure for now

--- chef-portfolio/src/App.js
+++ chef-portfolio/src/App.js
@@ -1,8 +1,40 @@
 import React from "react";
-import "./App.css";
+import {Route, NavLink, Switch, withRouter} from 'react-router-dom';
+import Homepage from "./components/Homepage";
+import PrivateRoute from "./components/PrivateRoute";
+// import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+  <nav>
+        <NavLink className="nav-home" to="/Homepage">
+            Home
+          </NavLink>
+        <NavLink className="nav-post" to ='/Recipes'>
+            Add_Recipe
+        </NavLink>
+        <NavLink className='nav-reg' to="/RegisterForm">
+            Register
+          </NavLink>
+        <NavLink className='nav-login' to="/LoginForm">
+              Login
+            </NavLink>
+  </nav>
+        
+  <section>
+          <Switch>
+            {/* <Route path="/login" component={LoginForm} />
+            <Route path="/registerform" component={RegisterForm} />
+            <Route path="/chefdashboard" render={props => <ChefDashBoard {...props} />} /> */}
+            <Route path="/homepage" component={Homepage} />
+            <PrivateRoute exact path = "/chefdashboard" />
+            {/* <Route exact path="/" render={() => <Redirect to="/HomePage" />} />
+            <Route exact path = '/post-form' component={Content} /> */}
+          </Switch>
+  </section>
+
+
+  </div>;
 }
 
 export default App;
---
+++ chef-portfolio/src/components/RecipesList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Recipes =()=> {
+    return(
+    <div>
+        RECIPES LIST COMING SOON
+    </div>
+    )
+}
+
+export default Recipes;
